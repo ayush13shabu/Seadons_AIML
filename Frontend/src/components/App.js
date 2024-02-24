487cc60a0606bc01fad7359b3ce858e95a4d2b27
ayush13shabu <81225749+ayush13shabu@users.noreply.github.com>
2024-02-24 19:14:04 +0530

    Initial frontend commit

---
+++ Frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import TravelForm from "./TravelForm";
+// import Footer from "./Footer";
+
+
+function App() {
+  
+  return (
+    <div>
+      <Header />
+      <TravelForm/>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <img 
+                alt="Trip Planner AI Logo" 
+                loading="lazy" 
+                width="29" 
+                height="29" 
+                decoding="async" 
+                data-nimg="1" 
+                className="mr-2 rounded-sm" 
+                src="https://tripplanner.ai/_next/image?url=%2Flogo%2Flogo.webp&amp;w=64&amp;q=75"
+            />            <h1>Travel_There</h1>
+        </header>
+    );
+}
+
+export default Header;
